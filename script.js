0abf0b2b545594414230dd8c9261821ea12ed469
Marcos <hrcln1974@gmail.com>
2026-05-20 22:36:45 -0300

    Primeiro site Atelier do Sabor

---
+++ script.js
@@ -0,0 +1,66 @@
+// STATUS DA LOJA
+
+const storeStatus = document.getElementById("storeStatus");
+
+const openBtn = document.getElementById("openStore");
+
+const closeBtn = document.getElementById("closeStore");
+
+
+// ABRIR LOJA
+
+openBtn.addEventListener("click", () => {
+
+  storeStatus.classList.remove("closed");
+
+  storeStatus.classList.add("open");
+
+  storeStatus.innerHTML =
+  "🟢 Loja Aberta • Aceitamos Encomendas";
+
+});
+
+
+// FECHAR LOJA
+
+closeBtn.addEventListener("click", () => {
+
+  storeStatus.classList.remove("open");
+
+  storeStatus.classList.add("closed");
+
+  storeStatus.innerHTML =
+  "🔴 Loja Fechada • Pedidos Pausados";
+
+});
+
+
+// PAUSAR PRODUTO
+
+const pauseButtons =
+document.querySelectorAll(".pause-btn");
+
+pauseButtons.forEach(button => {
+
+  button.addEventListener("click", () => {
+
+    const product =
+    button.parentElement;
+
+    product.classList.toggle("paused");
+
+    if(product.classList.contains("paused")){
+
+      button.innerHTML =
+      '<i class="fa-solid fa-eye"></i> Ativar Produto';
+
+    } else {
+
+      button.innerHTML =
+      '<i class="fa-solid fa-eye-slash"></i> Pausar Produto';
+
+    }
+
+  });
+
+});
